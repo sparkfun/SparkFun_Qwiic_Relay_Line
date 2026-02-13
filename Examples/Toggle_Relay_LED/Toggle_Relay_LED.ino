c65ac425779f2a4f112b4d19de62178bff290185
Nathan Seidle <nathan@sparkfun.com>
2026-02-13 13:50:36 -0700

    Fix polarized relay. Increase version on PCB and SCH.

---
+++ Examples/Toggle_Relay_LED/Toggle_Relay_LED.ino
@@ -0,0 +1,65 @@
+/*
+  Using the Qwiic Relay
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: February 12, 2026
+
+  License: This code is public domain, please do whatever you want!
+
+  Feel like supporting our work? Buy a board from SparkFun!
+  https://www.sparkfun.com/products/17047 - Qwiic GPIO
+  https://www.sparkfun.com/products/26784 - Qwiic Power Switch
+  https://www.sparkfun.com/products/26851 - Qwiic Directional Pad
+
+  This example demonstrates how to turn the relay on/off.
+
+  Hardware Connections:
+  Plug the Qwiic Relay into a Qwiic enabled board (SparkFun Thing Plus C, RedBoard, RedBoard IoT, etc)
+  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/17912)
+  Open the serial monitor at 115200 baud to see the output
+*/
+
+#include <SparkFun_I2C_Expander_Arduino_Library.h> // Click here to get the library: http://librarymanager/All#SparkFun_I2C_Expander_Arduino_Library
+
+//The Qwiic Relay Line uses a 16-bit GPIO expander, the PCA5555
+SFE_PCA95XX io(PCA95XX_PCA9555); // Create a PCA9555 instance
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(250);
+  Serial.println("SparkFun PCA95XX example");
+
+  Wire.begin();
+
+  // Initialize the PCA955x at its default address (0x20)
+  if (io.begin() == false)
+  {
+    Serial.println("PCA95xx not detected. Please check wiring. Freezing...");
+    while (true);
+  }
+
+  // pinMode can be used to set an I/O as OUTPUT or INPUT
+  for (int i = 0; i < 4; i++)
+  {
+    io.pinMode(i, OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (int i = 0; i < 1; i++)
+  {
+    io.digitalWrite(i, HIGH); // Turn pin HIGH
+    Serial.print("GPIO ");
+    Serial.print(i);
+    Serial.println(" is now high");
+    delay(2500);
+
+    io.digitalWrite(i, LOW); // Turn pin LOW
+    Serial.print("GPIO ");
+    Serial.print(i);
+    Serial.println(" is now low");
+    delay(2500);
+  }
+}
